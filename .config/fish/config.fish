52db5c3dd5809936f08ff061c579693bb7cd849e
Justin Oh <justinoh4116@gmail.com>
2023-07-28 06:55:50 -0400

    fzf.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,12 @@ set -gx GPG_TTY (tty)
 
 set -gx EDITOR nvim
 
-starship init fish | source
+set -gx fzf_fd_opts --no-ignore --hidden --exclude=.git
+set -gx fzf_directory_opts --bind "ctrl-o:execute(nvim {} &> /dev/tty)"
+set -gx fzf_preview_dir_cmd EXA -1 --color=always
+set -gx fzf_preview_file_cmd preview
+
+#starship init fish | source
 alias .. 'cd ..'
 abbr -a -- ~ 'cd ~'
 alias ll 'exa --long --header --git --icons'
